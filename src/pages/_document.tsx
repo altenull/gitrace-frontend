b55516a4f627d245035fd5ff495c3460cdc5ef6a
Heonyoung Kim <altenull@gmail.com>
2021-01-26 13:20:49 +0900

    feat(pages): bootstrap styled-components in order to apply styles at build time

---
+++ src/pages/_document.tsx
@@ -0,0 +1,48 @@
+import Document, { DocumentContext, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+interface Props {
+  styleTags: Array<React.ReactElement<{}>>;
+}
+
+export default class MyDocument extends Document<Props> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      // Retrieve styles from components in the page
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {/* Extract the styles as <style> tags */}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
