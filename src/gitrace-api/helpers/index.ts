248a9fbc7e1e2bbdaaf0646317aab5585b382ea1
Heonyoung Kim <altenull@gmail.com>
2021-02-13 15:19:06 +0900

    chore(gitrace-api): remove parseRepoDetailVM helper

--- src/gitrace-api/helpers/index.ts
+++ src/gitrace-api/helpers/index.ts
@@ -1,3 +1,2 @@
 export { default as parseCommitActivityVM } from './parseCommitActivityVM';
 export { default as parsePunchCardVM } from './parsePunchCardVM';
-export { default as parseRepoDetailVM } from './parseRepoDetailVM';
