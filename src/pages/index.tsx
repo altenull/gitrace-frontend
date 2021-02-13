c5d43b2bc7399f855b0a54db428830823a78c50e
Heonyoung Kim <altenull@gmail.com>
2021-02-13 15:20:07 +0900

    feat(pages): align SearchBox in home page to center of page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import { useRouter } from 'next/dist/client/router';
 import Head from 'next/head';
+import styled from 'styled-components';
 import { QueryParamKey } from '../core/enums/query-param-key.enum';
 import { SearchBox } from '../search/components';
+
+const StdMain = styled.main`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Home = () => {
   const router = useRouter();
@@ -22,9 +31,9 @@ const Home = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main>
+      <StdMain>
         <SearchBox placeholder='find github user' onEnter={navigateToSearchPage} />
-      </main>
+      </StdMain>
     </>
   );
 };
