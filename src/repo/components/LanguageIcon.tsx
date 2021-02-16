7ed9f9b2cd031216764d68274bfe4aec80928671
Heonyoung Kim <altenull@gmail.com>
2021-02-16 23:37:40 +0900

    fix(repo, ui): resolve type of Typescript to TypeScript

--- src/repo/components/LanguageIcon.tsx
+++ src/repo/components/LanguageIcon.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import { LanguageKey } from '../../core/enums/language-key.enum';
-import { HTMLIcon, JavaIcon, JavaScriptIcon, TypescriptIcon } from '../../ui';
+import { HTMLIcon, JavaIcon, JavaScriptIcon, TypeScriptIcon } from '../../ui';
 
 interface Props {
   language: LanguageKey;
@@ -9,7 +9,7 @@ interface Props {
 const LanguageIcon: React.FC<Props> = ({ language }: Props) => {
   const languageToIconMap: { [P in LanguageKey]: ReactNode } = {
     [LanguageKey.JavaScript]: <JavaScriptIcon />,
-    [LanguageKey.TypeScript]: <TypescriptIcon />,
+    [LanguageKey.TypeScript]: <TypeScriptIcon />,
     [LanguageKey.Java]: <JavaIcon />,
     [LanguageKey.HTML]: <HTMLIcon />,
   };
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,4 +2,4 @@ export { default as HTMLIcon } from './icons/HTMLIcon';
 export { default as JavaIcon } from './icons/JavaIcon';
 export { default as JavaScriptIcon } from './icons/JavaScriptIcon';
 export { default as SearchIcon } from './icons/SearchIcon';
-export { default as TypescriptIcon } from './icons/TypescriptIcon';
+export { default as TypeScriptIcon } from './icons/TypeScriptIcon';
