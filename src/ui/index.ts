ef929ca684cd4577c2b0ba9f81f0a9ce1035acae
Heonyoung Kim <altenull@gmail.com>
2021-02-17 18:52:42 +0900

    feat(ui): cretae Email, Location and People icon components

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,1 +1,4 @@
+export { default as EmailIcon } from './icons/EmailIcon';
+export { default as LocationIcon } from './icons/LocationIcon';
+export { default as PeopleIcon } from './icons/PeopleIcon';
 export { default as SearchIcon } from './icons/SearchIcon';
